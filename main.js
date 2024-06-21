5a9caf8801cd8c94ac909e1c86228ed94a890405
JoaoVictorBonan <128061508+JoaoVictorBonan@users.noreply.github.com>
2024-06-21 11:40:34 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
     }
     }
 console.log(botoes)
+const textos = document.querySelectorAll(".aba-conteudo");
+textos[i].classList.add("ativo");
+textos[j].classList.remove("ativo");
+const contadores = document.querySelectorAll(".contador");
+contadores[0].textContent = "Contagem regressiva";
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = tempoObjetivo1;
